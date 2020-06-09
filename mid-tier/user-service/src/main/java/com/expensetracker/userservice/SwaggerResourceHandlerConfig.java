d71e8816373a4b4d0498e30489c1fdbdf29d04c4
Sairam Harikrishnan <sai_hari98@hotmail.com>
2020-06-09 18:49:59 +0530

    Added Swagger UI 2.9.2 for User Service

---
+++ mid-tier/user-service/src/main/java/com/expensetracker/userservice/SwaggerResourceHandlerConfig.java
@@ -0,0 +1,17 @@
+package com.expensetracker.userservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class SwaggerResourceHandlerConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
