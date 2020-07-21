09c45479de0d0c905a1eded593c2b199103facb7
Sairam Harikrishnan <sai_hari98@hotmail.com>
2020-07-21 22:48:09 +0530

    Added Dto Request Java class

---
+++ mid-tier/expense-service/src/main/java/com/expensetracker/expenseservice/dto/AddIncomeRequestDto.java
@@ -0,0 +1,73 @@
+package com.expensetracker.expenseservice.dto;
+
+import java.util.Date;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.expensetracker.expenseservice.entity.IncomeCategory;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class AddIncomeRequestDto {
+
+	@Size(min = 1,max = 100, message = "Description is mandatory and should be within 100 characters")
+	private String description;
+	@Min(value = 1, message="Amount should be a positive value")
+	private int amount;
+	@Valid
+	private IncomeCategory incomeCategory;
+	@NotNull
+	@JsonFormat(pattern = "dd/MM/yyyy",shape = JsonFormat.Shape.STRING)
+	private Date date;
+
+	public AddIncomeRequestDto() {
+		super();
+	}
+
+	public AddIncomeRequestDto(
+			@Size(min = 1, max = 100, message = "Description is mandatory and should be within 100 characters") String description,
+			@Min(value = 1, message = "Amount should be a positive value") int amount,
+			@Valid IncomeCategory incomeCategory, Date date) {
+		super();
+		this.description = description;
+		this.amount = amount;
+		this.incomeCategory = incomeCategory;
+		this.date = date;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public IncomeCategory getIncomeCategory() {
+		return incomeCategory;
+	}
+
+	public void setIncomeCategory(IncomeCategory incomeCategory) {
+		this.incomeCategory = incomeCategory;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
