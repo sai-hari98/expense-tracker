e7509ef09aa3a84aa80768aefb69ac1e81ef2569
Sairam Harikrishnan <sai_hari98@hotmail.com>
2023-06-24 08:55:58 -0500

    added new entities

---
+++ mid-tier/expense-service/src/main/java/com/expensetracker/expenseservice/entity/PaymentMethod.java
@@ -0,0 +1,18 @@
+package com.expensetracker.expenseservice.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "payment-method")
+public class PaymentMethod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+}
---
+++ mid-tier/expense-service/src/main/java/com/expensetracker/expenseservice/entity/Transaction.java
@@ -0,0 +1,22 @@
+package com.expensetracker.expenseservice.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int transactionID;
+    private TransactionType type;
+    private double amount;
+    private TransactionCategory category;
+    private PaymentMethod paymentMethod;
+    private Card card;
+    private String remarks;
+}
